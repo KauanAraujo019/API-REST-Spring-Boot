5e02b46c378223f6da05faf1d34ba9f041ac23f4
Kauan Araujo <araujokauan664@gmail.com>
2025-10-27 10:30:30 -0300

    entidade category adicionada

--- src/main/java/com/SpringBoot/SpringProject/entities/Category.java
+++ src/main/java/com/SpringBoot/SpringProject/entities/Category.java
@@ -8,29 +8,24 @@ import jakarta.persistence.Id;
 import java.util.Objects;
 
 @Entity
-public class Product {
+public class Category {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
-    private String description;
-    private Double price;
-    private String imgUrl;
 
 
-    public Product(){
-
+    public Category(){
+        
     }
 
-    public Product(Long id, String name, String description, Double price, String imgUrl) {
+    public Category(Long id, String name) {
         this.id = id;
         this.name = name;
-        this.description = description;
-        this.price = price;
-        this.imgUrl = imgUrl;
     }
+
 
 
     public Long getId() {
@@ -49,41 +44,18 @@ public class Product {
         this.name = name;
     }
 
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Double getPrice() {
-        return price;
-    }
-
-    public void setPrice(Double price) {
-        this.price = price;
-    }
-
-    public String getImgUrl() {
-        return imgUrl;
-    }
-
-    public void setImgUrl(String imgUrl) {
-        this.imgUrl = imgUrl;
-    }
-
 
     @Override
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
-        Product product = (Product) o;
-        return Objects.equals(id, product.id);
+        Category category = (Category) o;
+        return Objects.equals(id, category.id);
     }
 
     @Override
     public int hashCode() {
         return Objects.hashCode(id);
     }
+
 
 }
