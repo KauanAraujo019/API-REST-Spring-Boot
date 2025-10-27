de391fb2b93d1c23fd976009ca02951f72308565
Kauan Araujo <araujokauan664@gmail.com>
2025-10-27 15:10:31 -0300

    resolvido problema ID categories

--- src/main/java/com/SpringBoot/SpringProject/controllers/CategoryController.java
+++ src/main/java/com/SpringBoot/SpringProject/controllers/CategoryController.java
@@ -5,6 +5,7 @@ import com.SpringBoot.SpringProject.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,7 +27,7 @@ public class CategoryController {
     }
 
     @GetMapping(value = "/{id}")
-    public ResponseEntity<Category> findById(Long id){
+    public ResponseEntity<Category> findById(@PathVariable Long id){
 
         return ResponseEntity.ok().body(categoryService.findById(id));
 
