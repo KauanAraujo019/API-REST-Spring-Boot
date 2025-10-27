875f339473a7f3f7e813e614b7511d19c2c3521b
Kauan Araujo <araujokauan664@gmail.com>
2025-10-27 14:02:18 -0300

    adicionado metodos GET em UserController

--- src/main/java/com/SpringBoot/SpringProject/controllers/UserController.java
+++ src/main/java/com/SpringBoot/SpringProject/controllers/UserController.java
@@ -1,11 +1,15 @@
 package com.SpringBoot.SpringProject.controllers;
 
+import com.SpringBoot.SpringProject.entities.User;
 import com.SpringBoot.SpringProject.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
+import java.util.List;
 
 @RestController
 @RequestMapping("/users")
@@ -15,7 +19,19 @@ public class UserController {
     private UserService userService;
 
 
+    @GetMapping
+    public ResponseEntity<List<User>> findAll(){
 
+        return ResponseEntity.ok().body(userService.findAll());
+
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id){
+
+        return ResponseEntity.ok().body(userService.findById(id));
+
+    }
 
 
 
