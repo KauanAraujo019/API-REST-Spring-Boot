6421f80cb3d3e0f8b554bd346bad643d8412c094
Kauan Araujo <araujokauan664@gmail.com>
2025-10-27 14:14:29 -0300

    adicionado metodos GET em OrderController

--- src/main/java/com/SpringBoot/SpringProject/services/OrderService.java
+++ src/main/java/com/SpringBoot/SpringProject/services/OrderService.java
@@ -1,4 +1,31 @@
 package com.SpringBoot.SpringProject.services;
 
+import com.SpringBoot.SpringProject.entities.Order;
+import com.SpringBoot.SpringProject.repositories.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class OrderService {
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+
+    public List<Order> findAll(){
+
+        return orderRepository.findAll();
+
+    }
+
+    public Order findById(Long id){
+
+        return orderRepository.findById(id).get();
+
+    }
+
+
+
 }
