a4cb464b0844a7dd24c24bb3fcb2fc4dd587b5f7
Kauan Araujo <araujokauan664@gmail.com>
2025-10-27 14:01:49 -0300

    UserController adicionada

---
+++ src/main/java/com/SpringBoot/SpringProject/controllers/UserController.java
@@ -0,0 +1,23 @@
+package com.SpringBoot.SpringProject.controllers;
+
+import com.SpringBoot.SpringProject.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+
+
+
+
+
+
+}
--- src/main/java/com/SpringBoot/SpringProject/services/UserService.java
+++ src/main/java/com/SpringBoot/SpringProject/services/UserService.java
@@ -2,14 +2,29 @@ package com.SpringBoot.SpringProject.services;
 
 import com.SpringBoot.SpringProject.entities.User;
 import com.SpringBoot.SpringProject.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
 
+    @Autowired
     private UserRepository userRepository;
 
+    public List<User> findAll(){
 
+        return userRepository.findAll();
+
+    }
+
+    public User findById(Long id){
+
+        return userRepository.findById(id).get();
+
+    }
 
 
 }
