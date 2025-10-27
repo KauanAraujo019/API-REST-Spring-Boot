85e65bf093b36c02f2b8e76eb02429300adfc518
Kauan Araujo <araujokauan664@gmail.com>
2025-10-27 15:07:55 -0300

    adicionado e implementado CategoryController

---
+++ src/main/java/com/SpringBoot/SpringProject/controllers/CategoryController.java
@@ -0,0 +1,36 @@
+package com.SpringBoot.SpringProject.controllers;
+
+import com.SpringBoot.SpringProject.entities.Category;
+import com.SpringBoot.SpringProject.services.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/categories")
+public class CategoryController {
+
+    @Autowired
+    private CategoryService categoryService;
+
+
+    @GetMapping
+    public ResponseEntity<List<Category>> findAll(){
+
+        return ResponseEntity.ok().body(categoryService.findAll());
+
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Category> findById(Long id){
+
+        return ResponseEntity.ok().body(categoryService.findById(id));
+
+    }
+
+
+}
