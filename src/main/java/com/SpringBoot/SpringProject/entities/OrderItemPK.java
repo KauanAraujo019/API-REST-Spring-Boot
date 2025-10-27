0a7b5f46bbdf3b9f7cfeef406a5fde3c59e4136c
Kauan Araujo <araujokauan664@gmail.com>
2025-10-27 16:20:30 -0300

    OrderItemPK implementado

--- src/main/java/com/SpringBoot/SpringProject/entities/OrderItemPK.java
+++ src/main/java/com/SpringBoot/SpringProject/entities/OrderItemPK.java
@@ -1,4 +1,49 @@
 package com.SpringBoot.SpringProject.entities;
 
-public class OrderItemPK {
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToMany;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@Embeddable
+public class OrderItemPK implements Serializable {
+
+    @ManyToMany
+    @JoinColumn(name = "id_order")
+    private Order order;
+
+    @ManyToMany
+    @JoinColumn(name = "id_product")
+    private Product product;
+
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        OrderItemPK that = (OrderItemPK) o;
+        return Objects.equals(product, that.product) && Objects.equals(order, that.order);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(product, order);
+    }
 }
